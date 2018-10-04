63da2434fb602885b9f5f1358ff6f973fc5e837c
Dell <Dell@192.168.0.61>
2018-10-04 12:52:10 +0530

    added reusable method dropdownlist

--- src/main/java/com/bankhrm/utils/seleniumhelper.java
+++ src/main/java/com/bankhrm/utils/seleniumhelper.java
@@ -1,5 +1,14 @@
 package com.bankhrm.utils;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
 public class seleniumhelper {
+	
+	public static void selectitemTextFromList(WebElement Dropdownitem ,String Dropdownitemtext)
+	{
+		Select list=new Select(Dropdownitem);
+		list.selectByVisibleText(Dropdownitemtext);
+	}
 
 }
